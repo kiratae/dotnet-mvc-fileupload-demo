127dccb7c49388e075a347f5f81cb7bc8019c413
tanaphon.k [PC] <tanaporn.k@clicknext.com>
2022-05-23 17:21:38 +0700

    update - google api demo

---
+++ Web/Controllers/MapController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace My.Demo.FileUpload.Web
+{
+    public class MapController : BaseController
+    {
+        private readonly ILogger<MapController> _logger;
+        public static readonly string Name = "Map";
+        public static readonly string ActionIndex = "Index";
+
+        public MapController(ILogger<MapController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
